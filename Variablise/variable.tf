24cc92a46abe655ee8d05402826bf0a5692ee3ee
sahare-rahul <151616061+sahare-rahul@users.noreply.github.com>
2023-12-15 21:07:05 +0530

    Update variable.tf

--- Variablise/variable.tf
+++ Variablise/variable.tf
@@ -1,10 +1,10 @@
 #in this variable we also putting vallue here
-variable "instance_type" {    --we can give any name here
+variable "instancetype" {    --we can give any name here
     type = string
     default = "t2.micro"
   
 }
 #second way we only varialise here and value will pick from terraform.tfvars
-variable "instance_type" {
+variable "instancetype" {
   type = string
 }
