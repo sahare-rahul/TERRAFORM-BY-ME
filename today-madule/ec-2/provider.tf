724b3d11dc55034addf70ed47e240fb742d4342c
sahare-rahul <151616061+sahare-rahul@users.noreply.github.com>
2023-12-18 20:18:59 +0530

    Update provider.tf

--- today-madule/ec-2/provider.tf
+++ today-madule/ec-2/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAZVLFURYR7UW6F2OI"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
