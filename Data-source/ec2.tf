edf28dc88b8d02b01ae69d4f4ddb89a8c8d56676
sahare-rahul <151616061+sahare-rahul@users.noreply.github.com>
2023-12-19 18:30:46 +0530

    Update ec2.tf

--- Data-source/ec2.tf
+++ Data-source/ec2.tf
@@ -7,9 +7,8 @@ provider "aws" {
 #creating ec2 passing data-soucrce of AMI
 
 resource "aws_instance" "my-instance" {
-  ami           = data.aws_ami.ubuntu.id      ---here we called created AMI by data-soucre to call data-sourec allwayes write "data." first
-  instance_type = "t3.micro"
-
+  ami           = data.aws_ami.ubuntu.id      ---here we called created AMI by data-soucre to call data-sourec allwayes write "data." first,
+  instance_type = "t3.micro"                      then in last put attribute .id for fefrence attribute may be defferent as per resource so 
   tags = {
     Name = "HelloWorld"
   }
