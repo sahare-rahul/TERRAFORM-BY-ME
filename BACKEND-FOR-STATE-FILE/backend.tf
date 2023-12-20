fe43f2a1e74e2f93a2252ec1f3df22b5bd01cbc6
sahare-rahul <151616061+sahare-rahul@users.noreply.github.com>
2023-12-20 16:41:26 +0530

    Update backend.tf

--- BACKEND-FOR-STATE-FILE/backend.tf
+++ BACKEND-FOR-STATE-FILE/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "backend-bucket-of-me"
-    key    = "backend/s3-backend.tfstate"
+    bucket = "backend-bucket-of-me"                 ----bucket-name
+    key    = "backend/s3-backend.tfstate"           ----here  "backend"  is folder in s3-bucket and s3-backend.tfstate is file name  
     region = "us-east-1"
     access_key = "AKIAZVLFURYRYVBWXSW4"
     secret_key = "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"
