dc72ee6541e6de9c2be070c66ec0b8b2a4f2d323
sahare-rahul <151616061+sahare-rahul@users.noreply.github.com>
2023-12-20 16:44:36 +0530

    2 deffrent ami with 2 defrre instance_type

--- new/ec-2.tf
+++ new/ec-2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 
 resource "aws_instance" "name" {
- count = length(var.ami) > length(var.instance_type) ? length(var.instance_type) : length(var.ami)
-    ami = element(var.ami, count.index)
+ count = length(var.ami) > length(var.instance_type) ? length(var.instance_type) : length(var.ami)  ----here we create 2 instances with 2 deffrent ami  
+    ami = element(var.ami, count.index                                                                   and instance types
     instance_type = element(var.instance_type, count.index)
 }
