614f44f79e59b3fa8deeed68dff79b3ec1bd2dab
sahare-rahul <151616061+sahare-rahul@users.noreply.github.com>
2024-05-28 23:38:11 +0530

    Update output.tf

--- OUTPUT/output.tf
+++ OUTPUT/output.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "my-instance" {
 # to print any parameter on terminal we use output
 
 output "instance_type" {                   here we printind instance type   "here we can giv any name"
-  value = aws_instance.my-instance_type
+  value = aws_instance.my-instance.instance_type
 }
 
 output "ami_id" {
